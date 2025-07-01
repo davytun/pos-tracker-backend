db6e6b3fdb36569fbc73e157457429e69add67bb
Davytun <davidakintunde433@gmail.com>
2025-07-01 20:43:29 +0100

    update

--- config/passport.js
+++ config/passport.js
@@ -12,21 +12,26 @@ passport.use(
       callbackURL: process.env.GOOGLE_CALLBACK,
     },
     async (accessToken, refreshToken, profile, done) => {
+      // Extra verification: Only allow users with a specific email domain
+      const allowedDomain = "gmail.com"; // Change this to your allowed domain
+      const userEmail = profile.emails[0].value;
+      if (!userEmail.endsWith(`@${allowedDomain}`)) {
+        // Fail authentication if not allowed
+        return done(null, false, { message: "Unauthorized domain" });
+      }
       let user = await prisma.user.findUnique({
         where: { googleId: profile.id },
       });
-
       if (!user) {
         user = await prisma.user.create({
           data: {
-            email: profile.emails[0].value,
+            email: userEmail,
             googleId: profile.id,
             name: profile.displayName,
             avatar: profile.photos[0].value,
           },
         });
       }
-
       done(null, user);
     }
   )
